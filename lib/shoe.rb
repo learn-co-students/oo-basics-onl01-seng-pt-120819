5d60a3b9c0a3cc4744ed9dad688f84cb742b50f9
Firas Chaabani <firaschaabani1@gmail.com>
2020-01-16 03:15:46 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class shoe
+  shoe.new ("Adidas")
+  shoe.new ("Nike")
+def brand (shoe)
+  puts shoe
+end
+def color (shoe)
+  puts color
+end
+def size (shoe)
+  puts size
+end
+def material (shoe)
+  puts material
+end
+def condition (shoe)
+  puts condition
+end
+
+  
